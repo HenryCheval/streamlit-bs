e456983510d199d3e1f5f4e3c389c37957019391
HenryCheval <875542931@qq.com>
2024-04-27 14:40:42 +0800

    4.27 requirements

--- homepage.py
+++ homepage.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Multipage App",
+    page_title="Learn AI more easily",
     page_icon="👋",
 )
 
-st.title("Main Page")
+st.title("An interactive and visual Machine Learning book")
 st.sidebar.success("Select a page above.")
